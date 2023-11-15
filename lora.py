6e34a811a11ff01b0491843a9434feb1289d6d43
Nathan Hu <nathu@iris-ws-16.stanford.edu>
2023-11-15 03:45:51 -0800

    rewrote lora via spliced in modules (previously functional)

--- lora.py
+++ lora.py
@@ -1,3 +1,4 @@
+#%%
 import torch
 import torch.nn as nn
 from torch.nn import Parameter
@@ -8,6 +9,70 @@ sys.path.append('../')
 from util import CACHE_DIR
 import re
 
+#wrapper class for linear layer
+class LoraLinear(nn.Module):
+    def __init__(self, original_linear, r):
+        super().__init__()
+        self.A = Parameter(torch.zeros(original_linear.weight.shape[0], r)) 
+        self.B = Parameter(torch.randn(r, original_linear.weight.shape[1]))
+        self.original_linear = original_linear
+        self.lora = True
+    
+    def forward(self, *args, **kwargs):
+        orig_out = self.original_linear(*args, **kwargs)
+        if self.lora:
+            #print([x.shape for x in args], [k for k in kwargs])
+            #print(self.original_linear.weight.shape, self.A.shape, self.B.shape, orig_out.shape)
+            
+            if isinstance(self.original_linear, transformers.models.gpt2.modeling_gpt2.Conv1D):
+                return orig_out + (args[0] @ self.A) @ self.B #check shapes
+            else:
+                return orig_out + (args[0]@ self.B.transpose(-1,-2)) @ self.A.transpose(-1,-2) #check shapes
+        else:
+            return orig_out
+
+#patch modules in place
+def add_lora(model, r):
+    for parent_module in list(model.modules()):
+        for name, module in parent_module.named_children():
+            if isinstance(module, nn.Linear) or isinstance(module, transformers.models.gpt2.modeling_gpt2.Conv1D):
+                setattr(parent_module, name, LoraLinear(module, r))
+
+#turn lora on or off via
+def set_lora_state(model, lora):
+    for module in list(model.modules()):
+        if isinstance(module, LoraLinear):
+            module.lora = lora
+
+def get_lora_parameters(model):
+    parameter_list = []
+    for module in list(model.modules()):
+        if isinstance(module, LoraLinear):
+            parameter_list.append(module.A)
+            parameter_list.append(module.B)
+    return parameter_list
+#set all non-lora params to not require gradient. Lora param's require_grad is set to lora_gradient 
+def freeze_non_lora(model, lora_gradient = True):
+    for param in model.parameters():
+        param.requires_grad = False
+    for param in get_lora_parameters(model):
+        param.requires_grad = lora_gradient
+  
+    
+    
+
+#%%
+
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+# from util import CACHE_DIR
+# model=AutoModelForCausalLM.from_pretrained('distilgpt2', cache_dir=CACHE_DIR)
+# add_lora(model, 2)
+# sample_input = torch.tensor([5,21,500,100])
+# out = model(sample_input)
+
+#%%
+
+#WE NO LONGER USE FUNCTIONAL IMPLEMENTATION OF LORA
 class LoraModel(nn.Module):
     def __init__(self, model, r, param_re = "transformer.h.(\d+).attn.(c_attn|c_proj).weight"):
         super().__init__()
@@ -16,6 +81,7 @@ class LoraModel(nn.Module):
         self.param_re = param_re
         self.lora_params = []
         self.lora_names = []
+        self.lora = True
         for name, p in self.base_model.named_parameters():
             if p.dim() == 2 and re.match(self.param_re, name):
                 A = Parameter(torch.zeros(p.shape[0], r))
@@ -27,13 +93,37 @@ class LoraModel(nn.Module):
             else:
                 self.lora_params.append(None)
         print(f'Initialized LORA parameters for {len(self.lora_names)} modules', self.lora_names)
+        
+        self.functional = make_functional(self.base_model)
         #self.lora_params = nn.ParameterList([p for p in self.lora_params if p is not None for p in p])
 
-    def forward(self, LORA=True, *args, **kwargs):
+
+    def adapt_parameters(self, model_parameters):
+        """
+        Adapt the parameters of the model to the LORA parameters.
+        model_parameters: an iterable of parameters
+        returns a list of parameters
+        """
+        new_params = [p if lp is None else p + lp[0] @ lp[1] for p, lp in zip(model_parameters, self.lora_params)]
+        return new_params
+        
+    def forward(self, LORA=None, base_parameters = None, *args, **kwargs):
+        """
+        A forward pass through the model.
+        LORA: whether to use LORA parameters, if None, use self.lora. When LORA is False, base_parameters is ignored and a forward pass is made through the base model.
+        base_parameters: if LORA is True, the base parameters to use. If None, use self.base_model.parameters()
+        """
+        
+        if LORA is None:
+            LORA = self.lora
+        
+        if base_parameters is None:
+            base_parameters = self.base_model.parameters()
+        elif not LORA:
+            print('Warning: LORA is False but base_parameters is not None. Ignoring base_parameters.')
         if LORA:
-            fmodel = make_functional(self.base_model)
-            new_params = [p if lp is None else p + lp[0] @ lp[1] for p, lp in zip(self.base_model.parameters(), self.lora_params)]
-            return fmodel(*args, **kwargs, params=new_params)
+            new_params = self.adapt_parameters(base_parameters)
+            return self.functional(*args, **kwargs, params=new_params)
         else:
             return self.base_model(*args, **kwargs)
 
@@ -56,3 +146,4 @@ class LoraModel(nn.Module):
                     original[0].data = loaded[0].data
                     original[1].data = loaded[1].data
         
+# %%
--- pretrain_lora.py
+++ pretrain_lora.py
@@ -3,22 +3,22 @@ import hydra
 import wandb
 from tqdm import tqdm
 from util import CACHE_DIR, get_base_model
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, Adafactor
 from doc_datasets import ArchivalQADataset
 import torch
 from hydra.utils import to_absolute_path
 import numpy as np
-from lora import LoraModel
+from lora import add_lora, set_lora_state, get_lora_parameters, freeze_non_lora
 
 #%%
 
-def validate_model(lora_model, val_dataloader, args):
+def validate_model(model, val_dataloader, args):
     print('validating...')
     losses = []
     with torch.no_grad():
         for batch in tqdm(val_dataloader):
             batch = {k: v.to(args.device) for k, v in batch.items()}
-            outputs = lora_model(input_ids = batch['qa_toks'], attention_mask = batch['qa_attn_mask'], labels = batch['qa_labels'], LORA = True)
+            outputs = model(input_ids = batch['qa_toks'], attention_mask = batch['qa_attn_mask'], labels = batch['qa_labels'])
             loss = outputs.loss
             losses.append(loss.item())
     return sum(losses)/len(losses)
@@ -27,31 +27,37 @@ def validate_model(lora_model, val_dataloader, args):
 @hydra.main(config_path='configs', config_name='pretrain_lora_config')
 def run(args):
     print('loading model...')
-    model = get_base_model(args.base_model, args.base_model_state_dict).to(args.device)
-    lora_model = LoraModel(model, args.r, args.param_re).to(args.device)
+    model = get_base_model(args.base_model, to_absolute_path(args.base_model_state_dict))
+    model.eval()
+    add_lora(model, args.r)
+    freeze_non_lora(model, lora_gradient = True)
+    model.to(args.device)
+    
     tokenizer = AutoTokenizer.from_pretrained(args.base_model, cache_dir = CACHE_DIR)
     tokenizer.pad_token_id = tokenizer.eos_token_id
     
     print('loading dataset...')
     if args.dataset.name == 'archivalqa':
-        train_dataset = ArchivalQADataset(to_absolute_path(args.dataset.qa_pretrain_data_path), tokenizer = tokenizer, max_length = args.dataset.max_length)
-        val_dataset = ArchivalQADataset(to_absolute_path(args.dataset.qa_pretrain_val_data_path), tokenizer = tokenizer, max_length = args.dataset.max_length)
+        train_dataset = ArchivalQADataset(to_absolute_path(args.dataset.qa_pretrain_data_path), tokenizer=tokenizer, full_passage=args.dataset.full_passage, max_length=args.dataset.max_length)
+        val_dataset = ArchivalQADataset(to_absolute_path(args.dataset.qa_pretrain_val_data_path), tokenizer=tokenizer, full_passage=args.dataset.full_passage, max_length=args.dataset.max_length)
     else:
         raise ValueError(f'Invalid dataset {args.dataset.name}')
     
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
     val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
-    
+    if args.debug:
+        sample_batch = next(iter(train_dataloader))
+        sample_batch = {k: v.to(args.device) for k, v in sample_batch.items()}
     wandb.init(project='un-camels', entity='nathu', job_type='lora_pretraining', config=args)
     
-    val_time_steps = [int(len(train_dataloader)*i/args.validations_per_epoch)-1 for i in range(args.validations_per_epoch)]
+    val_time_steps = [int(len(train_dataloader)*i/args.validations_per_epoch) for i in range(args.validations_per_epoch)] 
     
     best_val_loss = float('inf')
     
     if args.optimizer == 'adam':
-        optimizer = torch.optim.Adam(lora_model.lora_parameters(), lr=args.lr)
+        optimizer = torch.optim.Adam(get_lora_parameters(model), lr=args.lr)
     elif args.optimizer == 'adafactor':
-        optimizer = torch.optim.Adafactor(lora_model.lora_parameters(), lr=args.lr)
+        optimizer = Adafactor(model.parameters(), lr=args.lr, scale_parameter=False, relative_step=False)
     else:
         raise ValueError(f'Invalid optimizer {args.optimizer}')
     
@@ -60,7 +66,7 @@ def run(args):
         print(f'Epoch {epoch}')
         for i, batch in tqdm(enumerate(train_dataloader)):
             batch = {k: v.to(args.device) for k, v in batch.items()}
-            outputs = lora_model(input_ids = batch['qa_toks'], attention_mask = batch['qa_attn_mask'], labels = batch['qa_labels'], LORA = True)
+            outputs = model(input_ids = batch['qa_toks'], attention_mask = batch['qa_attn_mask'], labels = batch['qa_labels'])
             loss = outputs.loss
             losses.append(loss.item())
             loss = outputs.loss/args.gradient_accumulation_steps
@@ -69,22 +75,24 @@ def run(args):
             if (i+1) % args.gradient_accumulation_steps == 0:
                 optimizer.step()
                 optimizer.zero_grad()
-                with torch.no_grad():
-                    base_model_hash = np.mean([p.mean().item() for p in lora_model.base_model.parameters()])
-                
-                wandb.log({'train_loss': np.mean(losses), 
-                        'base_model_hash': base_model_hash})
+                if args.debug:
+                    with torch.no_grad():
+                        set_lora_state(model, False)
+                        base_model_loss = model(input_ids = sample_batch['qa_toks'], attention_mask = sample_batch['qa_attn_mask'], labels = sample_batch['qa_labels']).loss.item()
+                        wandb.log({'base_model_sample_loss': base_model_loss}, commit=False)
+                        set_lora_state(model, True)
+                wandb.log({'train_loss': np.mean(losses)})
                 
                 losses = [] 
                 
             if i in val_time_steps:
-                val_loss = validate_model(lora_model, val_dataloader, args)
+                val_loss = validate_model(model, val_dataloader, args)
                 wandb.log({'val_loss': val_loss})
                 if val_loss < best_val_loss:
                     best_val_loss = val_loss
-                    lora_model.save_lora(f'./best_lora.pt')
+                    torch.save(get_lora_parameters(model), f'./best_lora.pt')
                     print('Saved model')
-                lora_model.train()
+                
             
 if __name__ == '__main__':
     run()
